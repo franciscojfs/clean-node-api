8852ad444cc3237165b17af2d9e6428a9dc74377
Francisco Soares <francisco.soares@powerofdata.com.br>
2020-02-09 13:28:30 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,29 @@
 import { DbAddAccount } from './db-add-account'
+import { Encrypter } from '../../protocols/encrypter'
+
+interface SutTypes {
+  sut: DbAddAccount,
+  encrypterStub: Encrypter
+}
+
+const makeSut = (): SutTypes => {
+  class EncrypterStup {
+    async encrypt(value: string): Promise<string> {
+      return new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+
+  const encrypterStub = new EncrypterStup()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
-  test('Should call Encrypter with correct password', () => {
-    class EncrypterStup {
-      async encrypt(value: string): Promise<string> {
-        return new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-
-    const encrypterStub = new EncrypterStup()
-    const sut = new DbAddAccount(encrypterStub)
+  test('Should call Encrypter with correct password', () => {    
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
@@ -18,13 +32,7 @@ describe('DbAddAccount Usecase', () => {
 
     }
 
-    sut.add(accountData).then((value) => {
-
-    })
-    .catch((error) => {
-
-    })
-    
+    sut.add(accountData)
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
 })
